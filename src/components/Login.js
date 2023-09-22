c6208e3c03f9f3e13d85aac7ae81f7556745139e
Tomas Emr <tommy.emr@gmail.com>
2023-09-22 14:47:47 +0200

    login component

---
+++ src/components/Login.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+
+const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const url = `${process.env.REACT_APP_API_BASE_URL}/login`;
+
+        try {
+            const response = await axios.post(url, formData, { withCredentials: true });
+            navigate('/main');
+        } catch (error) {
+            const errorMsg = error.response?.data?.message || 'Error logging in';
+            alert(errorMsg);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="email" value={formData.email} onChange={handleChange} placeholder="Email" />
+                <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="Password" />
+                <button type="submit">Login</button>
+            </form>
+            <Link to="/register">Register new user!</Link>
+        </div>
+    );
+};
+
+export default Login;
